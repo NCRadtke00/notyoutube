83258a761d7f572204626a7bb2f83a2bf281dbd7
Nicholas Radtke <Raddnc@gmail.com>
2021-04-30 16:01:39 -0500

    finished a rough design for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./_header.scss";
 
-
 import { FaBars } from "react-icons/fa";
 import { AiOutlineSearch } from "react-icons/ai";
 import { MdNotifications, MdApps } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className="border border-dark">
-      header
+    <div className="header">
       <FaBars className="header__menu" size={26} />
       <img
         src="https://pngimg.com/uploads/youtube/youtube_PNG2.png"
