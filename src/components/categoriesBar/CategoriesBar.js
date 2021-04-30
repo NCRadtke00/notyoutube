78f3a16652459dd1710ae49a6f6cf776ad5b49bf
Nicholas Radtke <Raddnc@gmail.com>
2021-04-29 21:26:41 -0500

    created CategoriesBar.js and _categoriesBar.scss files,

---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './_categories.scss';
+const CategoriesBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CategoriesBar
