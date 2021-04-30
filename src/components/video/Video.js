b238c700bd74e857f09b0db77723120503b9eb8f
Nicholas Radtke <Raddnc@gmail.com>
2021-04-30 17:21:38 -0500

    outlining video

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./_video.scss";
+import { AiFillEye } from "react-icons/ai";
 const Video = () => {
-  return <div>video</div>;
+  return (
+    <div className="video">
+      <div className="video__top">
+        <img src="" alt="" />
+        <span>05:43</span>
+      </div>
+      <div className="video__title">Create app now with me nick</div>
+      <div className="video__details">
+        <span>
+          <AiFillEye /> Sm Views{" "}
+        </span>
+        <span>5 days ago</span>
+      </div>
+      <div className="video__channel">
+        <img src="" alt="" />
+        <p>Rainbow Hatter</p>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
