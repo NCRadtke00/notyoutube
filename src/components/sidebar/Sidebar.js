3430269c9c0aa1f5b19f20bd963bf520a61c172c
Nicholas Radtke <Raddnc@gmail.com>
2021-04-30 16:19:43 -0500

    working on sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,62 @@
-import React from 'react'
-import './_sidebar.scss';
-
+import React from "react";
+import "./_sidebar.scss";
+import {
+  MdSubscriptions,
+  MdExplore,
+  MdExitToApp,
+  MdThumbUp,
+  MdHistory,
+  MdLibraryBooks,
+  MdHome,
+  MdWatchLater,
+} from "react-icons/md";
 const Sidebar = () => {
-    return (
-        <div className="border border-danger">
-sidebar
-        </div>
-    )
-}
+  return (
+    <nav className="sidebar">
+      <li>
+        <MdHome size={23} />
+        <span>Home</span>
+      </li>
+      <li>
+        <MdExplore esize={23} />
 
-export default Sidebar
+        <span>Explore</span>
+      </li>
+
+      <li>
+        <MdSubscriptions size={23} />
+        <span>Subscriptions</span>
+      </li>
+      <hr />
+      <li>
+        <MdLibraryBooks size={23} />
+        <span>Library</span>
+      </li>
+      <li>
+        <MdHistory size={23} />
+        <span>History</span>
+      </li>
+      <li>
+        <MdWatchLater size={23} />
+        <span>Watch later</span>
+      </li>
+      <li>
+        <MdThumbUp size={23} />
+        <span>Liked Video</span>
+      </li>
+
+      <hr />
+        Subscriptions
+        <hr/>
+        MORE FROM YOUTUBE
+      <li>
+        <MdExitToApp size={23} />
+        <span>Log Out</span>
+      </li>
+
+      <hr />
+    </nav>
+  );
+};
+
+export default Sidebar;
