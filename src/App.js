ccb6256ca44cb89a82c307ed527536b84515363c
Nicholas Radtke <Raddnc@gmail.com>
2021-04-29 21:48:14 -0500

    removed dead file, updated index.js &app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
-
+import HomeScreen from "./screens/homescreen/HomeScreen";
 const App = () => {
   return (
     <>
       <Header />
       <div className="app_container">
         <Sidebar />
-        <Container className="app__main">
+        <Container fluid className="app__main border border-warning">
           <HomeScreen />
         </Container>
       </div>
