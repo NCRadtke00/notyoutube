0446cd37b6dac528884bb5e1bcd5062a80b263c4
Nicholas Radtke <Raddnc@gmail.com>
2021-05-02 18:50:50 -0500

    working on auth action and reducer

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,10 +1,50 @@
 import firebase from "firebase/app";
+
 import auth from "../../firebase";
+import {
+  LOAD_PROFILE,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOG_OUT,
+} from "../actionType";
 
 export const login = () => async (dispatch) => {
   try {
-    const provider = new firebase.autho.GoogleAuthProvider();
+    dispatch({
+      type: LOGIN_REQUEST,
+    });
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/youtube.force-ssl");
+
     const res = await auth.signInWithPopup(provider);
-    console.log(res);
-  } catch (errror) {}
+    const accessToken = res.credential.accessToken;
+
+    const profile = {
+      name: res.additionalUserInfo.profile.name,
+      photoURL: res.additionalUserInfo.profile.picture,
+    };
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: accessToken,
+    });
+    dispatch({
+      type: LOAD_PROFILE,
+      payload: profile,
+    });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.message,
+    });
+  }
+};
+export const log_out = () => async (dispatch) => {
+  await auth.signOut();
+  dispatch({
+    type: LOG_OUT,
+  });
 };
