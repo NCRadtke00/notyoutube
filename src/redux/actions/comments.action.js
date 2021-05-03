42a5bc889098c48b1da48ea12b793a6918023d18
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 16:57:20 -0500

    finished action for getting comments working on action to add comments

--- src/redux/actions/comments.action.js
+++ src/redux/actions/comments.action.js
@@ -1,20 +1,33 @@
-import request from '../../api'
+import request from "../../api";
 import {
-   COMMENT_LIST_FAIL,
-   COMMENT_LIST_REQUEST,
-   COMMENT_LIST_SUCCESS,
-   CREATE_COMMENT_FAIL,
-   CREATE_COMMENT_SUCCESS,
-} from '../actionType'
+  COMMENT_LIST_FAIL,
+  COMMENT_LIST_REQUEST,
+  COMMENT_LIST_SUCCESS,
+  CREATE_COMMENT_FAIL,
+  CREATE_COMMENT_SUCCESS,
+} from "../actionType";
 
-export const getCommentsOfVideosById = id => async dispatch => {
-    try{
-        
-    }catch(error){ 
-        console.log(error.response.data)
-        dispatch({
-        
-        })
+export const getCommentsOfVideoById = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: COMMENT_LIST_REQUEST,
+    });
 
-    }
-}
+    const { data } = await request("/commentThreads", {
+      params: {
+        part: "snippet",
+        videoId: id,
+      },
+    });
+    dispatch({
+      type: COMMENT_LIST_SUCCESS,
+      payload: data.items,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    dispatch({
+      type: COMMENT_LIST_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
