d3f3e46f557d92d8d3d98df219656504cd30eac6
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 17:40:16 -0500

    cleaned up reducer for video

--- src/redux/reducers/videos.reducer.js
+++ src/redux/reducers/videos.reducer.js
@@ -158,6 +158,39 @@ export const searchedVideosReducer = (
       return state;
   }
 };
+
+export const subscriptionsChannelReducer = (
+  state = {
+    loading: true,
+    videos: [],
+  },
+  action
+) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case SUBSCRIPTIONS_CHANNEL_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SUBSCRIPTIONS_CHANNEL_SUCCESS:
+      return {
+        ...state,
+        videos: payload,
+        loading: false,
+      };
+    case SUBSCRIPTIONS_CHANNEL_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
+
+    default:
+      return state;
+  }
+};
 export const selectedVideoReducer = (
   state = {
     loading: true,
@@ -183,38 +216,6 @@ export const selectedVideoReducer = (
       return {
         ...state,
         video: null,
-        loading: false,
-        error: payload,
-      };
-
-    default:
-      return state;
-  }
-};
-export const subscriptionsChannelReducer = (
-  state = {
-    loading: true,
-    videos: [],
-  },
-  action
-) => {
-  const { payload, type } = action;
-
-  switch (type) {
-    case SUBSCRIPTIONS_CHANNEL_REQUEST:
-      return {
-        ...state,
-        loading: true,
-      };
-    case SUBSCRIPTIONS_CHANNEL_SUCCESS:
-      return {
-        ...state,
-        videos: payload,
-        loading: false,
-      };
-    case SUBSCRIPTIONS_CHANNEL_FAIL:
-      return {
-        ...state,
         loading: false,
         error: payload,
       };
