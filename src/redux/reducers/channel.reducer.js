85776efd73637304e184f963b8025a10780a3f3a
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 16:49:46 -0500

    working on channel reducer

---
+++ src/redux/reducers/channel.reducer.js
@@ -0,0 +1,10 @@
+import {
+  CHANNEL_DETAILS_FAIL,
+  CHANNEL_DETAILS_REQUEST,
+  CHANNEL_DETAILS_SUCCESS,
+  SET_SUBSCRIPTION_STATUS,
+} from "../actionType";
+
+export const channelDetailsReducer = (state = {}, action) => {
+  const { payload, type } = action;
+};
