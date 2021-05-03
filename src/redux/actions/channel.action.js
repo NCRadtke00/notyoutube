c97da7fd655e783f287d0fc84d43ed06e41f49fa
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 16:54:59 -0500

    finished channel action

--- src/redux/actions/channel.action.js
+++ src/redux/actions/channel.action.js
@@ -8,5 +8,47 @@ import {
 
 export const getChannelDetails = (id) => async (dispatch) => {
   try {
-  } catch (error) {}
+    dispatch({
+      type: CHANNEL_DETAILS_REQUEST,
+    });
+
+    const { data } = await request("/channels", {
+      params: {
+        part: "snippet,statistics,contentDetails",
+        id,
+      },
+    });
+    dispatch({
+      type: CHANNEL_DETAILS_SUCCESS,
+      payload: data.items[0],
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    dispatch({
+      type: CHANNEL_DETAILS_FAIL,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const checkSubscriptionStatus = (id) => async (dispatch, getState) => {
+  try {
+    const { data } = await request("/subscriptions", {
+      params: {
+        part: "snippet",
+        forChannelId: id,
+        mine: true,
+      },
+      headers: {
+        Authorization: `Bearer ${getState().auth.accessToken}`,
+      },
+    });
+    dispatch({
+      type: SET_SUBSCRIPTION_STATUS,
+      payload: data.items.length !== 0,
+    });
+    console.log(data);
+  } catch (error) {
+    console.log(error.response.data);
+  }
 };
