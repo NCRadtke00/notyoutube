f1eabc35a9ac7c1d3eb82bf815e8cfc9b5c7821c
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 15:41:58 -0500

    finishing up on left side of component

--- src/components/videoHorizontal/VideoHorizontal.js
+++ src/components/videoHorizontal/VideoHorizontal.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./_videoHorizontal.scss";
+import { AiFillEye } from "react-icons/ai";
+import request from "../../api";
+import moment from "moment";
+import numeral from "numeral";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Col, Row } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const VideoHorizontal = ({ video }) => {
-  return <div>video</div>;
+  return (
+    <Row
+      className="py-2 m-1 videoHorizontal align-items-center"
+      onClick={handleClick}
+    >
+      <Col
+        xs={6}
+        md={searchScreen || subScreen ? 4 : 6}
+        className="videoHorizontal__left"
+      >
+        <LazyLoadImage
+          src={medium.url}
+          effect="blur"
+          className={`videoHorizontal__thumbnail ${thumbnail} `}
+          wrapperClassName="videoHorizontal__thumbnail-wrapper"
+        />
+        {isVideo && (
+          <span className="videoHorizontal__duration">{_duration}</span>
+        )}
+      </Col>
+      <Col></Col>
+    </Row>
+  );
 };
 
 export default VideoHorizontal;
