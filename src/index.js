0584c85dce37cee1dd37fd73578928381240b004
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 17:41:16 -0500

    finished index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import App from "./App";
 import "./_base.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
