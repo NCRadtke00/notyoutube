819bcdda682a7d22198fd8b4ef279b01f209e56d
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 15:04:06 -0500

    working on component for meta data

--- src/components/videoMetaData/VideoMetaData.js
+++ src/components/videoMetaData/VideoMetaData.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import "./videoMetaData.scss";
 function VideoMetaData() {
-    return (
-        <div>
-            
+  return (
+    <div className="videoMetaData_py-2">
+      <div className="videoMetaData__top">
+        <h5>Video title</h5>
+        <div className="py-1 d-flex justify-content-between align-items-center">
+          <span>
+            {numeral(viewCount).format("0.a")} Views •{" "}
+            {moment(publishedAt).fromNow()}
+          </span>
         </div>
-    )
+      </div>
+      <div className="videoMetaData__channel"> </div>
+      <div className="videoMetaData__description"></div>
+    </div>
+  );
 }
 
-export default VideoMetaData
+export default VideoMetaData;
