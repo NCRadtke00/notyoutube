77da4580c786ade58a8beb7573978d46693bc32d
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 18:15:07 -0500

    set layout for channelScreen

--- src/screens/channelScreen/ChannelScreen.js
+++ src/screens/channelScreen/ChannelScreen.js
@@ -1,9 +1,29 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import numeral from "numeral";
+import Video from "../../components/video/Video";
+import { getChannelDetails } from "../../redux/actions/channel.action";
+import { getVideosByChannel } from "../../redux/actions/videos.action";
 import "./channelScreen.scss";
 
-function ChannelScreen() {
-  return <div></div>;
+const ChannelScreen = () => {
+
+  return(
+    <div className="channelHeader">
+        <div className="d-flex">
+            <img src="" alt=""/>
+            <div className="channelHeader__deatails">
+                <h3></h3>
+                <span></span>
+            </div>
+        </div>
+        <button>Subscribe</button>
+    </div>
+    //copy and past container>row>Col>video
+  ) 
 }
 
 export default ChannelScreen;
