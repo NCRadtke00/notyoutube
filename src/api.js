24501c65de301cd427859c634f4b89ffbc427906
Nicholas Radtke <Raddnc@gmail.com>
2021-05-02 20:13:29 -0500

    created api.js to handle axios requests for youtube api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+console.log(provess.env.NCR_YTC_API_KEY);
+const request = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3/",
+  params: {
+    key: "",
+  },
+});
+export default request;
