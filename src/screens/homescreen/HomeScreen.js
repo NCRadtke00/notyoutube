16e2c2283a8bd2dc311f69d763dee1311b41ce25
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 13:44:51 -0500

    displaying all videos on homescreen

--- src/screens/homescreen/HomeScreen.js
+++ src/screens/homescreen/HomeScreen.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CategoriesBar from "../../components/categoriesbar/CategoriesBar";
 import Video from "../../components/video/Video";
+import { getPopularVideos } from "../../redux/actions/videos.action";
+import { useDispatch, useSelector } from "react-redux";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPopularVideos());
+  }, [dispatch]);
+  const { videos, activeCategory, loading } = useSelector(
+    (state) => state.homeVideos
+  );
+
   return (
     <Container>
       <CategoriesBar />
       <Row>
         {[...new Array(21)].map(() => (
           <Col Lg={3} md={4}>
-            <Video />
+            <Video video={video} key={video.id} />
           </Col>
         ))}
       </Row>
