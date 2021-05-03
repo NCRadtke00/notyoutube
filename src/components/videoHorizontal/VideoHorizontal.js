721396854e9c8c175abdbd41f474497844386d52
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 18:59:11 -0500

    finished VideoHorz component and added correct logic

--- src/components/videoHorizontal/VideoHorizontal.js
+++ src/components/videoHorizontal/VideoHorizontal.js
@@ -1,19 +1,85 @@
-import React from "react";
-import "./_videoHorizontal.scss";
-import { AiFillEye } from "react-icons/ai";
-import request from "../../api";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 import moment from "moment";
 import numeral from "numeral";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Col, Row } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+import { AiFillEye } from "react-icons/ai";
+import request from "../../api";
+import "./_videoHorizontal.scss";
 
-const VideoHorizontal = ({ video }) => {
+const VideoHorizontal = ({ video, searchScreen, subScreen }) => {
+  const {
+    id,
+    snippet: {
+      channelId,
+      channelTitle,
+      description,
+      title,
+      publishedAt,
+      thumbnails: { medium },
+      resourceId,
+    },
+  } = video;
+
+  const isVideo = !(id.kind === "youtube#channel" || subScreen);
+
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [channelIcon, setChannelIcon] = useState(null);
+
+  useEffect(() => {
+    const get_video_details = async () => {
+      const {
+        data: { items },
+      } = await request("/videos", {
+        params: {
+          part: "contentDetails,statistics",
+          id: id.videoId,
+        },
+      });
+      setDuration(items[0].contentDetails.duration);
+      setViews(items[0].statistics.viewCount);
+    };
+    if (isVideo) get_video_details();
+  }, [id, isVideo]);
+
+  useEffect(() => {
+    const get_channel_icon = async () => {
+      const {
+        data: { items },
+      } = await request("/channels", {
+        params: {
+          part: "snippet",
+          id: channelId,
+        },
+      });
+      setChannelIcon(items[0].snippet.thumbnails.default);
+    };
+    get_channel_icon();
+  }, [channelId]);
+
+  const seconds = moment.duration(duration).asSeconds();
+  const _duration = moment.utc(seconds * 1000).format("mm:ss");
+
+  const history = useHistory();
+
+  const _channelId = resourceId?.channelId || channelId;
+  //line about fixes inconsistance of youtube api
+  const handleClick = () => {
+    isVideo
+      ? history.push(`/watch/${id.videoId}`)
+      : history.push(`/channel/${_channelId}`);
+  };
+
+  const thumbnail = !isVideo && "videoHorizontal__thumbnail-channel";
+
   return (
     <Row
       className="py-2 m-1 videoHorizontal align-items-center"
       onClick={handleClick}
     >
+      {/* //TODO refractor grid */}
       <Col
         xs={6}
         md={searchScreen || subScreen ? 4 : 6}
