9bfbbf4f92443fac3def0d2dc80a3b9b6fda3d06
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 16:27:32 -0500

    working on channel actions

---
+++ src/redux/actions/channel.action.js
@@ -0,0 +1,12 @@
+import request from "../../api";
+import {
+  CHANNEL_DETAILS_FAIL,
+  CHANNEL_DETAILS_REQUEST,
+  CHANNEL_DETAILS_SUCCESS,
+  SET_SUBSCRIPTION_STATUS,
+} from "../actionType";
+
+export const getChannelDetails = (id) => async (dispatch) => {
+  try {
+  } catch (error) {}
+};
