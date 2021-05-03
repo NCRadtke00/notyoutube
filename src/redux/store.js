f444fef8c7f5828b0f80e23f52c7edb2db9edf0e
Nicholas Radtke <Raddnc@gmail.com>
2021-05-02 20:16:24 -0500

    added imports to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,12 @@
-const reducer = (initialState) => initialState;
+import { createStore, applyMiddleware, combineReducers } from "redux";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { authReducer } from "./reducers/auth.reducer";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
 const store = createStore(
   rootReducer,
   {},
