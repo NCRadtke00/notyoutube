46465f62ec9f906dfdc2236ea2da16eb52ecc7ae
Nicholas Radtke <Raddnc@gmail.com>
2021-05-03 13:55:12 -0500

    working on getting relatedvideos, and getbysearch

--- src/redux/actions/videos.action.js
+++ src/redux/actions/videos.action.js
@@ -35,57 +35,110 @@ export const getPopularVideos = () => async (dispatch, getState) => {
     });
   }
 };
-export const getVideosByCategory = keyword => async (dispatch, getState) => {
-    try {
-      dispatch({
-        type: HOME_VIDEOS_REQUEST,
-      });
-      const { data } = await request("/videos", {
-        params: {
-          part: "snippet, contextDetails, statistics",
-          chart: "mostPopular",
-          regionCode: "IN",
-          maxResults: 20,
-          pageToken: getState().homeVideos.nextPageToken,
-        },
-      });
-      dispatch({
-        type: HOME_VIDEOS_SUCCESS,
-        payload: {
-          videos: data.items,
-          nextPageToken: data.nextPageToken,
-          category: "ALL",
-        },
-      });
-    } catch (error) {
-      console.log(error.message);
-      dispatch({
-        type: HOME_VIDEOS_FAIL,
-        payload: error.message,
-      });
-    }
-  };
-  export const getVideoById = id => async dispatch => {
-    try {
-       dispatch({
-          type: SELECTED_VIDEO_REQUEST,
-       })
- 
-       const { data } = await request('/videos', {
-          params: {
-             part: 'snippet,statistics',
-             id: id,
-          },
-       })
-       dispatch({
-          type: SELECTED_VIDEO_SUCCESS,
-          payload: data.items[0],
-       })
-    } catch (error) {
-       console.log(error.message)
-       dispatch({
-          type: SELECTED_VIDEO_FAIL,
-          payload: error.message,
-       })
-    }
- }
+export const getRelatedVideos = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: RELATED_VIDEO_REQUEST,
+    });
+
+    const { data } = await request("/search", {
+      params: {
+        part: "snippet",
+        relatedToVideoId: id,
+        maxResults: 15,
+        type: "video",
+      },
+    });
+    dispatch({
+      type: RELATED_VIDEO_SUCCESS,
+      payload: data.items,
+    });
+  } catch (error) {
+    console.log(error.response.data.message);
+    dispatch({
+      type: RELATED_VIDEO_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+export const getVideosByCategory = (keyword) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: HOME_VIDEOS_REQUEST,
+    });
+    const { data } = await request("/videos", {
+      params: {
+        part: "snippet, contextDetails, statistics",
+        chart: "mostPopular",
+        regionCode: "IN",
+        maxResults: 20,
+        pageToken: getState().homeVideos.nextPageToken,
+      },
+    });
+    dispatch({
+      type: HOME_VIDEOS_SUCCESS,
+      payload: {
+        videos: data.items,
+        nextPageToken: data.nextPageToken,
+        category: "ALL",
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: HOME_VIDEOS_FAIL,
+      payload: error.message,
+    });
+  }
+};
+export const getVideoById = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: SELECTED_VIDEO_REQUEST,
+    });
+
+    const { data } = await request("/videos", {
+      params: {
+        part: "snippet,statistics",
+        id: id,
+      },
+    });
+    dispatch({
+      type: SELECTED_VIDEO_SUCCESS,
+      payload: data.items[0],
+    });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: SELECTED_VIDEO_FAIL,
+      payload: error.message,
+    });
+  }
+};
+export const getVideosBySearch = (keyword) => async (dispatch) => {
+  try {
+    dispatch({
+      type: SEARCHED_VIDEO_REQUEST,
+    });
+    const { data } = await request("/search", {
+      params: {
+        part: "snippet",
+
+        maxResults: 20,
+        q: keyword,
+        type: "video,channel",
+      },
+    });
+
+    dispatch({
+      type: SEARCHED_VIDEO_SUCCESS,
+      payload: data.items,
+    });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({
+      type: SEARCHED_VIDEO_FAIL,
+      payload: error.message,
+    });
+  }
+};
